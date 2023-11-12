dbef473401740572eb6f0994b12c3d9a3b35b924
tassianabenamor <tassiana.benamor@gmail.com>
2023-11-12 18:38:53 -0300

    Arquivo Main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import org.example.packageC.ClassC;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassC c = new ClassC();
+        c.methodC();
+    }
+}

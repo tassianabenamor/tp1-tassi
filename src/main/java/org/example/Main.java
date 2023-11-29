8d67f6926bc9c4e1f7b29c7018bd042b20fa3c32
tassianabenamor <tassiana.benamor@gmail.com>
2023-11-28 21:53:05 -0300

    fix de erros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,10 @@
 package org.example;
 import org.example.packageC.ClassC;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         ClassC c = new ClassC();
-        logger.info("logger info");
-        logger.debug("logger debug");
-        logger.error("logger error");
         c.methodC();
     }
 }
